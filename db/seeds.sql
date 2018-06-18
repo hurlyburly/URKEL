69a8b0f1694030ed29a6294e26f6a9b07abe44e9
Kalsang Bhutia <35473365+hurlyburly@users.noreply.github.com>
2018-06-18 18:37:40 -0500

    Set up file paths

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_names, devoured) VALUES ('Bacon Cheeseburger',FALSE);
+INSERT INTO burgers (burger_names, devoured) VALUES ('Veggie',FALSE);
+INSERT INTO burgers (burger_names, devoured) VALUES ('Double Cheeseburger',FALSE);
